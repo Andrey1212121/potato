3953c2deebdf6561f1787ec929b394782f2f211b
Andrey <andreyhara2005@gmail.com>
2024-07-10 15:34:12 +0300

    fix3

--- backend/router/index.js
+++ backend/router/index.js
@@ -2,12 +2,12 @@ const Router = require('express')
 const router_new = new Router()
 const potatoRouter = require('./potatoRouter')
 const userRouter = require('./userRouter')
-const typeRouter = require('./typeRouter')
+
 
 
 
 router_new.use('/user', userRouter, )
-//router_new.use('/type', typeRouter, )
+
 router_new.use('/potato', potatoRouter)
 
 
